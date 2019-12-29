80d2cd9b431ecd5d9f9152eaccbb7e5d91dcfd66
Jon Cram <jon@webignition.net>
2019-12-29 16:29:10 +0000

    Remove cruft

--- src/Factory.php
+++ src/Factory.php
@@ -153,40 +153,4 @@ class Factory
 
         return mb_substr($elementIdentifier, 0, $lastPositionDelimiterPosition - 1);
     }
-
-    /**
-     * @param string $identifierString
-     *
-     * @return array<int, int|string|null>
-     */
-    public static function extract(string $identifierString): array
-    {
-        $positionMatches = [];
-
-        preg_match(self::POSITION_REGEX, $identifierString, $positionMatches);
-
-        $position = null;
-
-        if (empty($positionMatches)) {
-            $quotedValue = $identifierString;
-        } else {
-            $quotedValue = (string) preg_replace(self::POSITION_REGEX, '', $identifierString);
-
-            $positionMatch = $positionMatches[0];
-            $positionString = ltrim($positionMatch, ':');
-
-            if (self::POSITION_FIRST === $positionString) {
-                $position = 1;
-            } elseif (self::POSITION_LAST === $positionString) {
-                $position = -1;
-            } else {
-                $position = (int) $positionString;
-            }
-        }
-
-        return [
-            $quotedValue,
-            $position,
-        ];
-    }
 }
