d5917717c0674c3c2e6a00fabafd73abeea4250e
Jon Cram <webignition@gmail.com>
2019-12-30 11:28:14 +0000

    Pass attribute name to AttributeIdentifier constructor (#31)

--- src/AttributeIdentifierInterface.php
+++ src/AttributeIdentifierInterface.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace webignition\DomElementIdentifier;
 
-use webignition\DomElementLocator\ElementLocatorInterface;
-
 interface AttributeIdentifierInterface extends ElementIdentifierInterface
 {
-    public function getAttributeName(): ?string;
-    public function withAttributeName(string $attributeName): ElementIdentifierInterface;
+    public function getAttributeName(): string;
 }
